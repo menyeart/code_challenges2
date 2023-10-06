1dd82c209e00a61b683c45c9de2bb5db19297143
Matt Enyeart <menyeart1@gmail.com>
2023-10-05 18:45:49 -0600

    Add additional test for numbers with abs value over 1 trillion

---
+++ AA_num_notation/notation_spec.rb
@@ -0,0 +1,39 @@
+require "./notation"
+require 'pry'
+
+RSpec.describe "AA Num Notation" do
+  xit "takes small integers and outputs 'AA' number notation" do
+    note = Notation.new
+
+    expect(note.convert(0)).to eq("0")
+    expect(note.convert(1)).to eq("1")
+    expect(note.convert(-1)).to eq("-1")
+  end
+
+  xit "takes small floats and outputs 'AA' number notation" do
+    note = Notation.new
+
+    expect(note.convert(0.25)).to eq("0.25")
+    expect(note.convert(0.009)).to eq("0")
+    expect(note.convert(-0.9999)).to eq("-0.99")
+  end
+
+  it "takes numbers with absolute values greater than 999 and up to 1 Trillion and outputs 'AA' number notation" do
+    note = Notation.new
+
+    # expect(note.convert(1000)).to eq("1K")
+    # expect(note.convert(1234)).to eq("1.23K")
+    expect(note.convert(-4002)).to eq("-4K")
+    expect(note.convert(100000)).to eq("100K")
+    expect(note.convert(123456789)).to eq("123M")
+    expect(note.convert(1234567890)).to eq("1.23B")
+    expect(note.convert(1234567890000)).to eq("1.23T")
+    expect(note.convert(999999999999999)).to eq("999T")
+  end
+
+  it "takes numbers with absolute values over 1 Trillion and outputs 'AA' number notation" do
+    note = Notation.new
+
+    expect(note.convert(1234567890000000000)).to eq("1.23ab")
+  end
+end
