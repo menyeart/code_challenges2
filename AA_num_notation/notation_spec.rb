aa354e97f75e3e092b8869b3f6247643553caefc
Matt Enyeart <menyeart1@gmail.com>
2023-10-10 10:59:37 -0600

    Add additional tests

--- AA_num_notation/notation_spec.rb
+++ AA_num_notation/notation_spec.rb
@@ -2,7 +2,7 @@ require "./notation"
 require 'pry'
 
 RSpec.describe "AA Num Notation" do
-  xit "takes small integers and outputs 'AA' number notation" do
+  it "takes small integers and outputs 'AA' number notation" do
     note = Notation.new
 
     expect(note.convert(0)).to eq("0")
@@ -10,7 +10,7 @@ RSpec.describe "AA Num Notation" do
     expect(note.convert(-1)).to eq("-1")
   end
 
-  xit "takes small floats and outputs 'AA' number notation" do
+  it "takes small floats and outputs 'AA' number notation" do
     note = Notation.new
 
     expect(note.convert(0.25)).to eq("0.25")
@@ -21,9 +21,10 @@ RSpec.describe "AA Num Notation" do
   it "takes numbers with absolute values greater than 999 and up to 1 Trillion and outputs 'AA' number notation" do
     note = Notation.new
 
-    # expect(note.convert(1000)).to eq("1K")
-    # expect(note.convert(1234)).to eq("1.23K")
+    expect(note.convert(1000)).to eq("1K")
+    expect(note.convert(1234)).to eq("1.23K")
     expect(note.convert(-4002)).to eq("-4K")
+    expect(note.convert(5809)).to eq("5.8K")
     expect(note.convert(100000)).to eq("100K")
     expect(note.convert(123456789)).to eq("123M")
     expect(note.convert(1234567890)).to eq("1.23B")
@@ -31,8 +32,9 @@ RSpec.describe "AA Num Notation" do
     expect(note.convert(999999999999999)).to eq("999T")
   end
 
-  it "takes numbers with absolute values over 1 Trillion and outputs 'AA' number notation" do
+  it "takes numbers with absolute values over 1000 Trillion and outputs 'AA' number notation" do
     note = Notation.new
+    expect(note.convert(1000000000000000)).to eq("1aa")
 
     expect(note.convert(1234567890000000000)).to eq("1.23ab")
   end
