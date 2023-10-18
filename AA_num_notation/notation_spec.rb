cebe5540b51df6a700587c3838be0f6d241528a7
Matt Enyeart <menyeart1@gmail.com>
2023-10-18 09:34:28 -0600

    Add additional tests for floats under 1000 and very large numbers

--- AA_num_notation/notation_spec.rb
+++ AA_num_notation/notation_spec.rb
@@ -2,7 +2,7 @@ require "./notation"
 require 'pry'
 
 RSpec.describe "AA Num Notation" do
-  it "takes small integers and outputs 'AA' number notation" do
+  xit "takes small integers and outputs 'AA' number notation" do
     note = Notation.new
 
     expect(note.convert(0)).to eq("0")
@@ -10,15 +10,17 @@ RSpec.describe "AA Num Notation" do
     expect(note.convert(-1)).to eq("-1")
   end
 
-  it "takes small floats and outputs 'AA' number notation" do
+  xit "takes small floats and outputs 'AA' number notation" do
     note = Notation.new
 
     expect(note.convert(0.25)).to eq("0.25")
     expect(note.convert(0.009)).to eq("0")
     expect(note.convert(-0.9999)).to eq("-0.99")
+    expect(note.convert(1.000000001)).to eq("1")
+    expect(note.convert(-740.069)).to eq("-740")
   end
 
-  it "takes numbers with absolute values greater than 999 and up to 1 Trillion and outputs 'AA' number notation" do
+  xit "takes numbers with absolute values greater than 999 and up to 1 Trillion and outputs 'AA' number notation" do
     note = Notation.new
 
     expect(note.convert(1000)).to eq("1K")
@@ -32,10 +34,17 @@ RSpec.describe "AA Num Notation" do
     expect(note.convert(999999999999999)).to eq("999T")
   end
 
-  it "takes numbers with absolute values over 1000 Trillion and outputs 'AA' number notation" do
+  xit "takes numbers with absolute values over 1000 Trillion and outputs 'AA' number notation" do
     note = Notation.new
-    expect(note.convert(1000000000000000)).to eq("1aa")
 
+    expect(note.convert(1000000000000000)).to eq("1aa")
     expect(note.convert(1234567890000000000)).to eq("1.23ab")
+  end
+
+  it "takes numbers with really large absolute values and outputs 'AA' number notation" do
+    note = Notation.new
+
+    expect(note.convert(1.23456789e+90)).to eq("unkown")
+    expect(note.convert(4.984071690388805e+92)).to eq("unknown")
   end
 end
