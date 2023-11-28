df51ab06cb7677ad97ac5c2d6bb465d9200ccc45
Matt Enyeart <menyeart1@gmail.com>
2023-11-28 08:23:17 -0700

    Add all artists and photos and get photos methods

--- museo/lib/curator.rb
+++ museo/lib/curator.rb
@@ -16,4 +16,14 @@ class Curator
   def find_artist_by_id(id)
     @artists.find { |artist| artist.id == id}
   end
+
+  def all_artists_and_photos
+    artists_and_photos = {}
+    @artists.each { |artist| artists_and_photos[artist.name] = get_photos(artist) }
+    artists_and_photos
+  end
+
+  def get_photos(artist)
+    @photographs.find_all { |photograph| artist.id == photograph.artist_id}.map { |photo| photo.name}
+  end
 end
