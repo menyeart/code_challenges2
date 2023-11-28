84b76202ea353409656a50ed2b40b6f44c9c6295
Matt Enyeart <menyeart1@gmail.com>
2023-11-28 09:17:45 -0700

    Create artists age and other photos method

--- museo/lib/curator.rb
+++ museo/lib/curator.rb
@@ -52,4 +52,17 @@ class Curator
     @photographs.find_all { |photo| photo.year.between?(start_year, end_year)}
   end
 
+  def get_artists_age_and_other_photos(photo)
+    year = photo.year.to_i
+    artist = @artists.find { |artist| artist.id == photo.artist_id}
+    artist_age = year - artist.born.to_i
+    other_photos = @photographs.find_all do |photog|
+      photog.year == photo.year && photog.artist_id == photo.artist_id && photo.id != photog.id
+    end
+    other_photos = other_photos.map { |photo| photo.name }
+
+    other_photos == [] ? other_photos = "none" : other_photos = other_photos.join
+
+    "#{artist.name} was #{artist_age}yrs old when #{photo.name} was taken. Other photos from that year include: #{other_photos}."
+  end
 end
