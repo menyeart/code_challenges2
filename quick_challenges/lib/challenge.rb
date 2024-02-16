c220fe5a1d5eb6f204e0afcf7deff6a022f1e5ce
Matt Enyeart <menyeart1@gmail.com>
2024-02-16 11:16:51 -0700

    Complete rounders challenge

--- quick_challenges/lib/challenge.rb
+++ quick_challenges/lib/challenge.rb
@@ -2,16 +2,31 @@ class Challenge
   def initialize
   end
 
-  # Return 0 if the array is null, empty or less than 3 elements
-  # Find the smallest number remove it
-  # Find the largest number remove it
-  # Sum up the rest of the array
-
   def sum_without_min_max(array)
     return 0 if array == nil || array.count <= 3
-
     sum_extremes = array.min + array.max
-
     array.sum - sum_extremes
   end
+
+  # Start with the last digit(1s spot) round it using round half up
+  # Go through ascending spot(10s, 100s, 1000s etc) rounding each one 
+  # Stop rounding and return the number when there is one no zero digit left
+
+  def rounders(int)
+    divisor = 10.0
+    num = int
+      until  check_if_rounded(num) == true do
+      num  = (num/divisor).round
+      num = (num * divisor).to_i
+      divisor *= 10
+    end
+    num
+  end
+
+  def check_if_rounded(num)
+    num_arr = num.to_s.split("")
+    num_hash = num_arr.tally
+    return true if num_hash["0"] == num_arr.count - 1
+  end
+
 end
--- quick_challenges/spec/challenge_spec.rb
+++ quick_challenges/spec/challenge_spec.rb
@@ -1,4 +1,4 @@
-require './lib/challenges'
+require './lib/challenge'
 require 'rspec'
 require 'pry'
 
@@ -20,9 +20,15 @@ RSpec.describe "Quick codewars challenges" do
     expect(challenge.sum_without_min_max([-3,-5])).to eq(0)
     expect(challenge.sum_without_min_max([6, 2, 1, 8, 10])).to eq(16)
     expect(challenge.sum_without_min_max([-6, -20, -1, -10, -12])).to eq(-28)
-  
   end
 
+  it "#rounders" do
+    challenge =  Challenge.new
 
-
+    expect(challenge.rounders(15)).to eq(20)
+    expect(challenge.rounders(1234)).to eq(1000)
+    expect(challenge.rounders(1445)).to eq(2000)
+    expect(challenge.rounders(99)).to eq(100)
+    expect(challenge.rounders(10)).to eq(10)
+  end
 end
